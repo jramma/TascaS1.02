e91f29d865b38675a94f10ee4cc08c27bb7f2b6d
Pepe <107991714+jramma@users.noreply.github.com>
2022-06-29 10:09:23 +0200

    Add files via upload

---
+++ src/n2ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package n2ejercicio1;
+
+//import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		byte a = sc.nextByte();
+//
+//		System.out.println(a);
+//		sc.close();
+	
+	System.out.println(Entrada.leerSiNo("Introduce"));
+	
+	}
+}
